50d1d498075fbff0bb217b9f590f3ddd60b2dc51
Shawn Taylor <shawncurtistaylor@gmail.com>
2019-09-07 15:04:03 -0700

    san diego fix

--- main.js
+++ main.js
@@ -1,4 +1,19 @@
 $(document).ready(function() {
-// write your code here
+    // write your code here
+$.getJSON("data.json", function(data){
+    var sDData = '';
+    $.each(data, function(key,value){
+        sDData += '<tr>';
+        sDData += '<td>'+value.name;
+        sDData += '<td>'+value.description+'</td>';
+        sDData += '<td><a href="https://www.google.com/maps?q=' + value.location + '">'+value.location+'</a></td>';
+        sDData += '</tr>'; 
 
+    });
+
+   $('#tableData').append(sDData);
+   console.log(sDData);
 });
+
+
+});  
